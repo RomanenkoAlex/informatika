66a99a056ed366dfb3685d1fd9f996e5cffc3641
RomanenkoAlex <144201982+RomanenkoAlex@users.noreply.github.com>
2023-09-20 11:04:14 +0500

    Update lab1.py

--- labs/lab1.py
+++ labs/lab1.py
@@ -1,4 +1,10 @@
-import math 
+import math as m # импортировал модуль математика
 
 def task1():
-  pass
+  x = float(input('Введи x'))
+  y = float(input('Введи y'))
+  z = float(input('Введи z'))
+  
+  a = (m.sin(x)-m.cos(y)**2)/(m.sin(x)**2*m.cos(y)**2)
+  b = abs(z)**(1/4)+m.cos(y)* sin(x)**2+m.log(1+y, 2)
+
